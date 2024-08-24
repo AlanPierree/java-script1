2b181961a6370cbbf5c01c5aa29f827c1adc93bd
AlanPierree <161715146+AlanPierree@users.noreply.github.com>
2024-08-24 20:01:51 -0300

    Add files via upload

---
+++ 2-trabalhandoComVariáveis.js
@@ -0,0 +1,9 @@
+console.log("Trabalhando com variáveis");
+// JS é case-sensitive
+// Console é diferente de console
+
+// Sempre declarar a variável
+const idade = 15;
+const nome = "Alan";
+
+console.log("Alan tem", idade, "anos.")
