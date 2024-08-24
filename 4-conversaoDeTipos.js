8a6cd0433f200e1a1ccdd30be75e2ad489efa5db
AlanPierree <161715146+AlanPierree@users.noreply.github.com>
2024-08-24 20:55:05 -0300

    Add files via upload

---
+++ 4-conversaoDeTipos.js
@@ -0,0 +1,14 @@
+console.log("Conversão de Tipos");
+
+// Soma de texto + número ou texto + texto resultam em uma junção
+// Soma entre textos é chamada de concatenação
+console.log("ano" + 2020);
+console.log("2" + "2");
+console.log(parseInt("2") + parseInt("2"));
+// parseInt faz a conversão de tipos de variável
+
+console.log("10" / "2")
+console.log("Alan" / "2") // NaN - Not a Number
+
+console.log(6.5) // Para decimais, usa-se ponto ao invés de vírgula
+console.log(6,5) // Se usado vírgula, os valores são separados
