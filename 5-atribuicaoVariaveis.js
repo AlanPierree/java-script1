fc65f17f9ad6501ed16d07915058025a1665d129
AlanPierree <161715146+AlanPierree@users.noreply.github.com>
2024-08-25 17:28:11 -0300

    Add files via upload

---
+++ 5-atribuicaoVariaveis.js
@@ -0,0 +1,18 @@
+console.log("Atribuição de Variáveis")
+
+const idade = 15;
+// let (permitir, no inglês) declara uma variável que pode ser alterada
+const primeiroNome = "Alan";
+const sobrenome = "Pierre";
+
+let contador = 0;
+contador = contador + 1;
+
+console.log(primeiroNome, "tem", idade, "anos.");
+// Variáveis podem ser atribuídas dentro de strings usando "${}"
+// Crase (`) também pode ser usado para declarar string
+console.log(`Meu nome é ${primeiroNome} ${sobrenome}`);
+
+nomeCompleto = (primeiroNome + sobrenome);
+
+console.log(nomeCompleto);
---
+++ 6-listas.js
@@ -0,0 +1,14 @@
+console.log(`Trabalhando com listas`);
+
+// const salvador = `Salvador`;
+// const saoPaulo = `São Paulo`;
+// const rioDeJaneiro = `Rio de Janeiro`;
+
+// new Array cria uma lista
+const listaDeDestinos = new Array(
+    `Salvador`, `São Paulo`, `Rio de Janeiro`
+);
+
+listaDeDestinos.push(`Curitiba`) // Adiciona novo item à lista
+
+console.log(listaDeDestinos)
