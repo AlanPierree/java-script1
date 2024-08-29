c39e4c46495d00ce8901b4048788c28c88975a42
AlanPierree <161715146+AlanPierree@users.noreply.github.com>
2024-08-29 08:36:03 -0300

    Add files via upload

--- 7-condicionais.js
+++ 7-condicionais.js
@@ -7,16 +7,25 @@ listaDeDestinos.push(`Curitiba`); // push adiciona novo item à lista
 
 const idadeComprador = 18;
 const estaAcompanhada = true;
+const temPassagemComprada = true;
 
-console.log(`Lista de destinos:`);
+console.log(`Destinos possíveis:`);
 console.log(listaDeDestinos);
 
-if (idadeComprador >= 18) {
-    console.log("Comprador maior de idadade");
+if (idadeComprador >= 18 || estaAcompanhada == true) {
+    console.log("Comprador maior de idade/acompanhado");
     listaDeDestinos.splice(1, 1); // Removendo item
-}   else if (estaAcompanhada == true) {
+} else if (estaAcompanhada == true) {
     console.log("Comprador está acompanhado");
     listaDeDestinos.splice(1, 1); // Removedo item
 } else {
     console.log("Não é maior de idade e não posso vender");
+}
+
+console.log(`Embarque: \n\n`)
+
+if (idadeComprador >= 18 && temPassagemComprada) {
+    console.log(`Boa viagem!`)
+} else {
+    console.log(`Você não pode embarcar.`)
 }
