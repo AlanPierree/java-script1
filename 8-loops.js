90a3ea0116d500962fa6ccaa01d46ba213785dd2
AlanPierree <161715146+AlanPierree@users.noreply.github.com>
2024-09-10 08:47:06 -0300

    Add files via upload

--- 8-loops.js
+++ 8-loops.js
@@ -1,29 +1,29 @@
 console.log(`Trabalhando com condicionais`);
-const listaDeDestinos = new Array(`Salvador`, `São Paulo`, `Rio de Janeiro`);
+const listaDeDestinos = new Array(
+  `Salvador`,
+  `São Paulo`,
+  `Rio de Janeiro`
+);
 
 listaDeDestinos.push(`Curitiba`); // push adiciona novo item à lista
 
-const idadeComprador = 18;
+const idadeComprador = 15;
 const estaAcompanhada = true;
 let temPassagemComprada = false;
+const destino = "Salvador";
 
-console.log(`Destinos possíveis:`);
+
+console.log(`\nDestinos possíveis:`);
 console.log(listaDeDestinos);
 
-if (idadeComprador >= 18 || estaAcompanhada == true) {
-    console.log("Comprador maior de idade/acompanhado");
-    listaDeDestinos.splice(1, 1); // Removendo item
-}   else if (estaAcompanhada == true) {
-    console.log("Comprador está acompanhado");
-    listaDeDestinos.splice(1, 1); // Removedo item
-} else {
-    console.log("Não é maior de idade e não posso vender");
-}
+const podeComprar = idadeComprador >= 18 || estaAcompanhada == true;
 
-console.log(`Embarque: \n\n`)
-
-if(idadeComprador >= 18 && temPassagemComprada) {
-  console.log(`Boa viagem!`)
-} else{
-  console.log(`Você não pode embarcar.`)
+let contador = 0;
+while (contador < 3) {
+  if (listaDeDestinos[contador] == destino) {
+    console.log("Destino existe")
+  } else {
+    console.log("Destino não existe");
+  }
+  contador += 1;
 }
